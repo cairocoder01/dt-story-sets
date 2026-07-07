1de62cde15d317cb567b7b1344f5aefd27219f68
DT Loop Engineer <loop-engineer[bot]@disciple.tools>
2026-07-07 10:27:43 +0300

    wip(#1): iteration 1 — agent execution

--- test/install-wp-tests.sh
+++ test/install-wp-tests.sh
@@ -109,8 +109,12 @@ install_test_suite() {
 	if [ ! -d $WP_TESTS_DIR ]; then
 		# set up testing suite
 		mkdir -p $WP_TESTS_DIR
-		svn co --quiet https://develop.svn.wordpress.org/${WP_TESTS_TAG}/tests/phpunit/includes/ $WP_TESTS_DIR/includes
-		svn co --quiet https://develop.svn.wordpress.org/${WP_TESTS_TAG}/tests/phpunit/data/ $WP_TESTS_DIR/data
+		# use git instead of svn
+		TAG=${WP_TESTS_TAG#*/}
+		git clone --depth 1 --branch ${TAG} https://github.com/WordPress/wordpress-develop.git /tmp/wordpress-develop
+		cp -r /tmp/wordpress-develop/tests/phpunit/includes/ $WP_TESTS_DIR/includes
+		cp -r /tmp/wordpress-develop/tests/phpunit/data/ $WP_TESTS_DIR/data
+		rm -rf /tmp/wordpress-develop
 	fi
 
 	if [ ! -f wp-tests-config.php ]; then
